ee5996913e2d379916704429db2c563bf2cb83ae
itsAbhayRaj <ay0218757@gmail.com>
2025-08-11 23:49:20 +0530

    utils discussion

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import dotenv from 'dotenv'
-import mongoose from "mongoose"
-import express from "express"
-import { DB_NAME } from "./constants.js"
-import connectDB from "./db/index.js"
-
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
-  path: './.env'
-})
+  path: "./.env",
+});
 
 connectDB()
+  .then(() => {
+    const server = app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is live on localhost:${process.env.PORT}`);
+    });
+    server.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+  })
+  .catch((err) => {
+    console.log("MONOGO DB Connection Failed: ", err);
+  });
 
+  app.get("/", (req, res) => {
+    res.send("Hello");
+  })
 // console.log("Hello");
-
 
 /*
 // function connectDB(){}
