98fa5d6649da68066eff5fb4fe742a7183e30a47
itsAbhayRaj <ay0218757@gmail.com>
2025-08-25 19:38:24 +0530

    feat: add routing and controller files for health check, comments, likes, playlists, dashboard, subscriptions, and tweets

--- src/app.js
+++ src/app.js
@@ -15,9 +15,24 @@ app.use(express.static('public'));
 app.use(cookieParser());
 
 // routes import
+import healthCheckRouter from './routes/healthCheck.routes.js';
 import userRouter from './routes/user.routes.js';
+import videoRouter from './routes/video.routes.js';
+import commentRouter from './routes/comment.routes.js';
+import likeRouter from './routes/like.routes.js';
+import playlistRouter from './routes/playlist.routes.js';
+import dashboardRouter from './routes/dashboard.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import tweetRouter from './routes/tweet.routes.js';
 
 // routes declaration
+app.use('/api/v1/healthCheck', healthCheckRouter);
 app.use('/api/v1/users', userRouter);
-
+app.use('/api/v1/videos', videoRouter)
+app.use('/api/v1/comments', commentRouter);
+app.use('/api/v1/likes', likeRouter);
+app.use('/api/v1/playlist', playlistRouter);
+app.use('/api/v1/dashboard', dashboardRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/tweets', tweetRouter);
 export default app;
---
+++ src/controllers/video.controller.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+import asyncHandler from "../utils/asyncHandler.js";
+
+const getAllVideos = asyncHandler(async(req,res) =>{
+    const {page = 1,limit = 10, query, sortBy, sortType, userId } = req.query;
+    
+})
